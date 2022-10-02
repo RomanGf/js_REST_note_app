c041a287019b24e653c702028f897fea2b631295
RomanGf <gafyak.roma@gmail.com>
2022-10-02 16:59:37 +0300

    add docker

--- src/app.ts
+++ src/app.ts
@@ -8,5 +8,5 @@ app.use(express.json());
 
 app.use("/notes", notesRouters);
 
-const port: string | number = process.env.PORT || 3000;
+const port: string | number = process.env.PORT || 9000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
